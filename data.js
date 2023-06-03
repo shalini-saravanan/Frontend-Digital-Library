d9ceed4d0de323bde4fbd94cbcdc0388c7306a39
Shalini S <20eucs127@skcet.ac.in>
2023-06-03 10:17:22 +0530

    Added books page

---
+++ data.js
@@ -0,0 +1,66 @@
+const data = [
+  {
+    name: "jjawe",
+    author: "bhji",
+    tags: ["vgy", "nji"],
+  },
+  {
+    name: "2ajawe",
+    author: "2abhji",
+    tags: ["avgy", "anji"],
+  },
+  {
+    name: "3jawe",
+    author: "3bhji",
+    tags: ["vgy", "nji"],
+  },
+  {
+    name: "4ajawe",
+    author: "4abhji",
+    tags: ["-avgy", "anji"],
+  },
+  {
+    name: "5jawe",
+    author: "5bhji",
+    tags: ["vgy", "nji"],
+  },
+  {
+    name: "6ajawe",
+    author: "6abhji",
+    tags: ["avgy", "anji"],
+  },
+  {
+    name: "7jawe",
+    author: "7bhji",
+    tags: ["Crime", "nji"],
+  },
+  {
+    name: "8ajawe",
+    author: "8abhji",
+    tags: ["RomCom", "anji"],
+  },
+  {
+    name: "9jawe",
+    author: "9bhji",
+    tags: ["vgy", "nji"],
+  },
+  {
+    name: "11ajawe",
+    author: "11abhji",
+    tags: ["avgy", "anji"],
+  },
+  {
+    name: "22j22awe",
+    author: "22bhji",
+    tags: ["vgy", "nji"],
+  },
+  {
+    name: "33ajawe",
+    author: "33abhji",
+    tags: ["avgy", "anji"],
+  },
+];
+
+const authors = ["Jane Austen", "33abhji"];
+
+const tags = ["Crime", "RomCom", "Novel", "avgy"];
